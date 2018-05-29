c0ecc0c790922979f02fa34a01bc4776816c3d05
eselse <bpytc23up@gmail.com>
2018-05-29 21:41:04 +0400

    from sublime

--- assets/JS/todos.js
+++ assets/JS/todos.js
@@ -1,1 +1,30 @@
-console.log("all system works fine");
+// $('li').click(function(){
+// 	// if li is gray
+// 	if($(this).css("color") === "rgb(128, 128, 128)") {
+// 		// turn it black
+// 		$(this).css({
+// 			color: "black",
+// 			textDecoration: "none"
+// 		});
+// 	}
+// 	else{
+// 		// turn it gray
+// 		$(this).css({
+// 			color: "gray",
+// 			textDecoration: "line-through"
+// 		});
+// 	}
+// });
+
+// Check Off Specific Todos By Clicking
+$("li").click(function(){
+	$(this).toggleClass('done');
+});
+
+// Click on X to delete todo
+$("span").click(function(event){
+    $(this).parent().fadeOut(550, function() {
+    	$(this).remove();
+    });
+    event.stopPropagation();
+});
